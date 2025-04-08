0881f30adc4b47c5a8289a3ea1b058da1d1e8243
Rousselin Thomas <98778784+Devattom@users.noreply.github.com>
2025-04-08 13:23:35 +0200

    ajout TP 22 (#5)
    Co-authored-by: Thomas ROUSSELIN <t.rousselin@mtarget.fr>

--- TP/src/peter/javabnb/Main.java
+++ TP/src/peter/javabnb/Main.java
@@ -4,6 +4,7 @@ import peter.javabnb.logements.Appartement;
 import peter.javabnb.logements.Chalet;
 import peter.javabnb.logements.Logement;
 import peter.javabnb.logements.Maison;
+import peter.javabnb.outils.JavaBnBData;
 import peter.javabnb.outils.MaDate;
 import peter.javabnb.outils.Utile;
 import peter.javabnb.reservations.*;
@@ -14,18 +15,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-
         // Déclaration & Initialisation
         Voyageur monVoyageur = new Voyageur("Peter", "Bardu", 32);
-        Hote monHote1 = new Hote("Tom", "Martin", 30, 12);
-        Hote monHote2 = new Hote("Emma", "Wilson", 52, 1);
-
-        //Logement monLogement1 = new Logement(monHote1, "Maison au calme", 80, "3 rue des logements", 100, 4);
-
-        Maison maMaison = new Maison(monHote1, "Petite Maison", 60, "12 rue des maisons", 70, 4, 1000, true);
-        Appartement monAppartement = new Appartement(monHote2, "Grand appartement", 150, "4 rue des apparts", 120, 6, 10, 4);
-        Chalet monChalet = new Chalet(monHote1, "Chalet de montagne", 130, "5 rue de la montagne", 50, 6, 1345);
-
 
         // Les critères de mon séjour
         Date maDate = new MaDate(11, 4, 2025);
@@ -33,7 +24,8 @@ public class Main {
 
         int nbNuits = Utile.choix("le nombre de nuits", 1, 31);
 
-        Logement logement = monChalet;
+        Logement logement = JavaBnBData.getInstance().getLogements().get(0);
+
         int nbVoyageurs = Utile.choix("le nombre de voyageurs", 1, 12);
 
         // Création d'un séjour (court ou long)
